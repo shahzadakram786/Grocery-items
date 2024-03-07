3daca67e9f822bef6879319059a2188a4937589e
shahzad akram <alyakram786@gmail.com>
2024-03-07 22:24:30 +0500

    pop almost complete now working on del text

--- script.js
+++ script.js
@@ -1,24 +1,43 @@
 let doc = document.getElementById("main-2");
+let popup = document.querySelector(".pop")
 let input = document.getElementById("input")
 let addbutton = document.getElementById("Add-button");
-let inputCheck = document.getElementById("checkB")
+let p = document.getElementById("p")
+let iR = document.getElementById("iR")
+let iG = document.getElementById("iG")
+let line = document.getElementById("line")
+let inph3 = document.getElementById("text-h")
 
 
-
-    let Value = input.value;
+   let value = input.value;
 
 addbutton.addEventListener('click', ()=>{
 
-    if(input.value === ""){
+  
+    setTimeout(function(){
+        popup.style.display = 'none';
+    },2000 )
+
+    if(input.value == ""){
         console.log("Please provide a value");
+        iR.style.display = 'block';
+        iG.style.display = 'none';
+        p.innerText = "Please provide a value"
+        popup.style.display = 'block';
+        
+
+        
     }
-    else {
-        console.log("provided a value", Value);
+    else if(!input.value == ""){
+        console.log("provided a value", input.value);
+        popup.style.display = 'block';
+        iR.style.display = 'none';
+        iG.style.display = 'block';
 
       doc.innerHTML += `
       <div class="inner">
-      <input type="checkbox" id="checkB" onclick="checked()" >
-      <h3 id="ih3">${Value}</h3>
+      <input type="checkbox" id="checkB" onclick="myFunction()">
+      <h3 id="text-h">${input.value}</h3>
       <button class="delete" onclick="delete()">Delete</button>
       </div>
       `
@@ -27,10 +46,17 @@ addbutton.addEventListener('click', ()=>{
 
 })
 
-function checked(){
-    if(inputCheck.checked === true){
-        input.value.style.text-decoration-line = "line-through";
-        
-    }
+function myFunction(){ 
+let inputCheck = document.getElementById("checkB");
 
+      console.log("checked")
+    if(inputCheck.checked == true){
+        console.log("checked22")
+      
+    }
+    else{
+        // console.log("unchecked", value)
+        // input.value.innerText += `<del>${input.value}</del>`;
+    }
+// myFunction(checked).
 }
